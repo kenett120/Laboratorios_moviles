f8aaec0659c7bac5c2604e758f501513fcab7156
kenett120 <kenettcrack7@gmail.com>
2026-08-19 13:06:48 -0600

    Adding work

--- Lab4/app/src/main/java/plat/lab1/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/plat/lab1/lab4/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import plat.lab1.lab4.ui.theme.Lab4Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,108 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LabScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LabScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .border(
+                8.dp,
+                Color(0xFF146B2E)
+            )
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(310.dp),
+            painter = painterResource(id = R.drawable.escudo_uvg),
+            contentDescription = "Escudo UVG",
+            alpha = 0.22f
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 250.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Universidad del Valle\nde Guatemala",
+                textAlign = TextAlign.Center,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 42.sp
+            )
+            Spacer(
+                modifier = Modifier.height(20.dp)
+            )
+            Text(
+                text = "Programación de plataformas\nmóviles, Sección 30",
+                textAlign = TextAlign.Center,
+                fontSize = 25.sp,
+                lineHeight = 36.sp
+            )
+            Spacer(
+                modifier = Modifier.height(25.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 50.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "INTEGRANTES",
+                    fontWeight = FontWeight.Bold
+                )
+
+                Column {
+                    Text("Kenett Ortega")
+                    Text("Junior Lancério")
+                    Text("Axel Xitumul")
+                }
+            }
+
+            Spacer(
+                modifier = Modifier.height(20.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 50.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "CATEDRÁTICO",
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Juan Carlos Durini"
+                )
+            }
+
+            Spacer(
+                modifier = Modifier.height(35.dp)
+            )
+
+            Text("Kenett Alexander Ortega Cerón")
+            Text("25777")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab4Theme {
-        Greeting("Android")
+        LabScreen()
     }
 }
